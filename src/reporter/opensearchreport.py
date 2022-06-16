3013e571349149510093e27f3a34bf146944f9fe
Armijn Hemel <armijn@tjaldur.nl>
2022-06-16 16:38:52 +0200

    opensearch: further rewrite

--- src/reporter/opensearchreport.py
+++ src/reporter/opensearchreport.py
@@ -23,14 +23,53 @@
 '''
 Write results to an instance of OpenSearch.
 '''
-
 import copy
+
 import opensearchpy
+import click
+
+# import YAML module
+from yaml import load
+try:
+    from yaml import CSafeLoader as Loader
+except ImportError:
+    from yaml import Loader
 
 
 class OpenSearchReporter:
     def __init__(self, environment):
-        self.environment = environment
+        self.environment = copy.deepcopy(environment)
+
+        if self.environment['opensearch_port'] is None:
+            self.environment['opensearch_port'] = 9200
+        else:
+            if not isinstance(self.environment['opensearch_port'], int):
+                self.environment['opensearch_port'] = 9200
+
+        if self.environment['opensearch_host'] is None:
+            self.environment['opensearch_host'] = 'localhost'
+
+        if self.environment['opensearch_index'] is None:
+            self.environment['opensearch_index'] = ''
+
+        if self.environment['opensearch_user'] is None:
+            self.environment['opensearch_user'] = ''
+
+        if self.environment['opensearch_password'] is None:
+            self.environment['opensearch_password'] = ''
+
+        # Create the client with SSL/TLS enabled, but hostname verification disabled.
+        self.client = opensearchpy.OpenSearch(
+            hosts = [{'host': host, 'port': port}],
+            http_compress = True, # enables gzip compression for request bodies
+            http_auth = auth,
+            # client_cert = client_cert_path,
+            # client_key = client_key_path,
+            use_ssl = False,
+            verify_certs = False,
+            ssl_assert_hostname = False,
+            ssl_show_warn = False
+        )
 
     def report(self, scanresult):
         '''Put results into OpenSearch'''
@@ -46,38 +85,6 @@ class OpenSearchReporter:
 
         uuid = result['session']['uuid']
 
-        # first create the OpenSearch connection string. TODO: sanitize
-        connectionstring = 'http://%s:%s@%s:%d/'
-
-        if self.environment['opensearch_port'] is None:
-            opensearch_port = 9200
-        else:
-            opensearch_port = self.environment['opensearch_port']
-
-        if self.environment['opensearch_host'] is None:
-            opensearch_host = 'localhost'
-        else:
-            opensearch_host = self.environment['opensearch_host']
-
-        if self.environment['opensearch_index'] is None:
-            opensearch_index = ''
-        else:
-            opensearch_index = self.environment['opensearch_index']
-
-        if self.environment['opensearch_user'] is None:
-            opensearch_user = ''
-        else:
-            opensearch_user = self.environment['opensearch_user']
-
-        if self.environment['opensearch_password'] is None:
-            opensearch_password = ''
-        else:
-            opensearch_password = self.environment['opensearch_password']
-
-        connectionstring = connectionstring % (opensearch_user, opensearch_password, opensearch_host, opensearch_port)
-
-        es = elasticsearch.OpenSearch([connectionstring])
-
         # store some information about the session
         es.index(index=opensearch_index, doc_type='_doc', body=result['session'])
 
@@ -90,3 +97,23 @@ class OpenSearchReporter:
 
     def update(self, scanresult):
         pass
+
+@click.command(short_help='load results into OpenSearch')
+@click.option('--config-file', '-c', required=True, help='configuration file', type=click.File('r'))
+def main(config_file):
+    # read the configuration file. This is in YAML format
+    try:
+        environment = load(config_file, Loader=Loader)
+    except:
+        print("Cannot open configuration file, exiting", file=sys.stderr)
+        sys.exit(1)
+    # some sanity checks:
+    if 'config' not in config:
+        print("Invalid configuration file, exiting", file=sys.stderr)
+        sys.exit(1)
+
+    # create an OpenSearch reporter object
+    reporter = OpenSearchReporter(environment)
+
+if __name__ == "__main__":
+    main()
